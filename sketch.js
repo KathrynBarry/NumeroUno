e7b061f8b0649d688a1dbb18d2eb117ec7ccc7bc
KathrynBarry <barryk3@tcd.ie>
2016-10-06 14:36:56 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+function setup() {
+  createCanvas(500,500);
+    background(0)
+ 
+}
+
+function draw() {
+  fill(0);
+  stroke(205,50,180);
+  ellipse(240,240,25,25);
+  
+  fill(0,245,255);
+  ellipse(235,235,5,5);
+  ellipse(245,235,5,5);
+  fill(255,50,10);
+  ellipse(240,245,6,5);
+  
+  fill(0);
+  ellipse(240,265,25,28);
+  fill(25,220,25);
+  noStroke();
+  rect(235,280,3,3);
+   fill(25,220,25);
+  noStroke();
+  rect(240,280,3,3);
+  
+  stroke(255,8,8);
+  line(220,255,230,265);
+  line(250,255,260,245);
+  
+  
+
+  
+ 
+  
+  
+  
+}
